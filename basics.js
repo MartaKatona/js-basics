1cd6871df908d111644f76c634d9955acdc4fc25
Crescendi <martahonolulu@hotmail.com>
2016-09-06 16:48:59 -1000

    first excersise done

--- basics.js
+++ basics.js
@@ -1,4 +1,5 @@
 /* Create a `myName` variable and assign it a String value */
+var myName = 'Today is a rainy day';
 
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
